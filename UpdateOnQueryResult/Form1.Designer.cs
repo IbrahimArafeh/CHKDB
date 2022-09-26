fb1abc9d8c199b0cff3eae8bf001a998fc1356ea
Ibrahim Arafeh <ibrahimarafeh5@gmail.com>
2022-09-26 17:34:47 +0400

    Upload First project

---
+++ UpdateOnQueryResult/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UpdateOnQueryResult
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lbReadingValues = new System.Windows.Forms.ListBox();
+            this.lbResultValues = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(237, 41);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(284, 67);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lbReadingValues
+            // 
+            this.lbReadingValues.FormattingEnabled = true;
+            this.lbReadingValues.Location = new System.Drawing.Point(60, 146);
+            this.lbReadingValues.Name = "lbReadingValues";
+            this.lbReadingValues.Size = new System.Drawing.Size(185, 264);
+            this.lbReadingValues.TabIndex = 1;
+            // 
+            // lbResultValues
+            // 
+            this.lbResultValues.FormattingEnabled = true;
+            this.lbResultValues.Location = new System.Drawing.Point(492, 146);
+            this.lbResultValues.Name = "lbResultValues";
+            this.lbResultValues.Size = new System.Drawing.Size(185, 264);
+            this.lbResultValues.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbResultValues);
+            this.Controls.Add(this.lbReadingValues);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ListBox lbReadingValues;
+        private System.Windows.Forms.ListBox lbResultValues;
+    }
+}
+
---
+++ UpdateOnQueryResult/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace UpdateOnQueryResult
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // get all gender values from DB
+        public void getValues()
+        {
+            
+            List<int> values = new List<int>();
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=TestDB;Integrated Security=True");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("Select * from Persons", conn);
+            
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int eee = (int)reader["Gender"];
+                    if(eee != 2)
+                    {
+                        getValues();
+                    }
+                }
+            }
+            
+            conn.Close();
+            Thread.Sleep(2000);
+        }
+
+        // check if all values is 2
+        public bool checkValues(List<int> listOfValues)
+        {
+            bool result = true;
+            foreach(int value in listOfValues)
+            {
+                if(value != 2)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            getValues();
+        }
+    }
+}
